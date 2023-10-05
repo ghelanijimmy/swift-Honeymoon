0327717745f26f757ae46b0ba666fadafc6305ea
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-10-05 09:00:21 -0400

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Jimmy Ghelani on 2023-10-05.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
