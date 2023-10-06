033d2498e3aaf112935ee2f625e2010f41a2f809
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-10-06 08:23:17 -0400

    footer view

---
+++ Honeymoon/Views/FooterView.swift
@@ -0,0 +1,55 @@
+//
+//  FooterView.swift
+//  Honeymoon
+//
+//  Created by Jimmy Ghelani on 2023-10-06.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack {
+            Button(action: {
+                
+            }) {
+                Image(systemName: "xmark.circle")
+                    .font(.system(size: 42, weight: .light))
+            }
+            .tint(.primary)
+            
+            Spacer()
+            
+            Button(action: {}) {
+                Text("Book Destination".uppercased())
+                    .font(.system(.subheadline, design: .rounded))
+                    .fontWeight(.heavy)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 12)
+                    .tint(.pink)
+                    .background(
+                        Capsule()
+                            .stroke(.pink, lineWidth: 2)
+                    )
+            }
+            
+            Spacer()
+            
+            Button(action: {
+                
+            }) {
+                Image(systemName: "heart.circle")
+                    .font(.system(size: 42, weight: .light))
+            }
+            .tint(.primary)
+        } //: HSTACK
+        .padding()
+    }
+}
+
+#Preview {
+    FooterView()
+}
